086cc6be42551cdf3d4927050ae1841a7ec5a58b
Ana Monteiro <aninhamonteiro2010@gmail.com>
2022-02-16 08:58:19 -0300

    requisição post

--- config/customExpress.js
+++ config/customExpress.js
@@ -2,8 +2,10 @@ const express = require('express');
 const consign = require('consign');
 
 module.exports = () => {
-
     const app = express();
+
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json);
 
 consign()
     .include('controllers')
