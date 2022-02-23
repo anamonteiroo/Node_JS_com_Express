fed7b121e8ea4367601d4c1f9914b835a5803bd5
Ana Monteiro <aninhamonteiro2010@gmail.com>
2022-02-23 15:26:55 -0300

    criando tabela

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
 const Tabelas = require('./infraestrutura/tabelas');
 
+const porta = 3000 
 conexao.connect(erro => {
     if(erro) {
         console.log(erro);
@@ -12,7 +13,7 @@ conexao.connect(erro => {
 
         const app = customExpress();
         
-        app.listen(3000, () => console.log('Servidor rodando na porta 3000')); 
+        app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`)); 
     };
 });
  
