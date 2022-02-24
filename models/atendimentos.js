57381155a30dffb921db2fea18f54c78d4b8d74c
Ana Monteiro <aninhamonteiro2010@gmail.com>
2022-02-23 21:44:17 -0300

    rota listagem

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,7 +6,32 @@ class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
         const data = moment(atendimento.data).format('YYYY-MM-DD HH:mm:ss');
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual.'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres.'
+            }
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json(erros);
+        } else {
+
         const atendimentoDatado = {...atendimento, dataCriacao, data};
+
         const sql = 'INSERT INTO Atendimentos SET ?';
 
         conexao.query(sql, atendimentoDatado, (erro, resultados) => {
@@ -14,6 +39,20 @@ class Atendimento {
                 res.status(400).json(erro);
             } else {
                 res.status(201).json(resultados);
+            };
+        });
+
+        };
+    };
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos';
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
             };
         });
     };
